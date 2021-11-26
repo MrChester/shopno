f6c84ecfe22ce2156b316c0a73c70bf3d5f1e242
Yauheni Ramankou <iromankov2010@gmail.com>
2021-11-26 16:13:59 +0300

    fix: add listing to pop-up, fix markup and styles

--- src/js/pop-up.js
+++ src/js/pop-up.js
@@ -1,40 +1,82 @@
-let galleryItemList = document.querySelectorAll('.works-gallery__item');
+let container = document.querySelector('.container');
+let worksImgList = document.querySelectorAll('.works-gallery__img');
 let popUp = document.getElementById('pop-up');
 let popUpImg = document.getElementById('pop-up__img');
+var leftControl = document.querySelector('.pop-up__control--left');
+var closeControl = document.querySelector('.control__icon--close');
+var rightControl = document.querySelector('.pop-up__control--right');
 let screenPercent = 0.75;
+let windowOuterWidth = window.screen.width;
+var containerLeftPadding = parseInt(window.getComputedStyle(container, null).getPropertyValue('padding-left'));
+var containerRightPadding = parseInt(window.getComputedStyle(container, null).getPropertyValue('padding-right'));
+let containerWidth = container.clientWidth;
+let slideIndex = 0;
+var containerPadding = containerLeftPadding + containerRightPadding;
 
-for (let i = 0; i < galleryItemList.length; i++)
-{
-    galleryItemList[i].addEventListener('click', function (event) {
-        let screenHeight = document.documentElement.clientHeight;
-        let screenWidth = document.documentElement.offsetWidth;
-        const src = event.target.getAttribute('src');
-
-        popUpImg.setAttribute('src', src);
+for (let i = 0; i < worksImgList.length; i++) {
+    worksImgList[i].addEventListener('click', function (event) {
+        slideIndex = parseInt(event.target.getAttribute('slide-index'));
+        containerWidth = container.clientWidth - containerPadding;
+        currentSlide(slideIndex);
         popUp.classList.remove('fade');
-
-        if (screenWidth <= 540)
-        {
-            popUpImg.style.maxWidth = (screenPercent * screenWidth) + 'px';
-        }
-        else if (screenWidth >= 540 && screenWidth < 768)
-        {
-            popUpImg.style.maxHeight = (screenPercent * screenHeight) + 'px';
-        }
-        else
-        {
-            popUpImg.style.maxWidth = (screenPercent * screenHeight) + 'px';
-        }
+        setImgWidth(containerWidth, screenPercent);
         popUpImg.classList.remove('fade');
         popUpImg.classList.add('show');
     });
 }
 
-popUp.addEventListener('click', () =>
-{
+closeControl.addEventListener('click', (event) => {
     popUp.classList.add('fade');
     popUpImg.removeAttribute('style');
     popUpImg.classList.remove('show');
     popUpImg.classList.add('fade');
     popUpImg.setAttribute('src', '#');
 });
+
+leftControl.addEventListener('click', () => {
+    previousSlide();
+});
+
+rightControl.addEventListener('click', () => {
+    nextSlide();
+});
+
+function setImgWidth(containerWidth, screenPercent) {
+    let windowHeight = window.innerHeight;
+    let maxWidth = screenPercent * containerWidth;
+    if (containerWidth < windowHeight) {
+        popUpImg.style.maxWidth = (maxWidth) + 'px';
+    } else {
+        if (containerWidth >= (windowOuterWidth / 2)) {
+            popUpImg.style.maxWidth = (screenPercent * windowHeight) + 'px';
+        } else {
+            popUpImg.style.maxWidth = (maxWidth) + 'px';
+        }
+    }
+}
+
+function showSlides(n) {
+    let i;
+    let slide = popUpImg;
+
+    if (n > worksImgList.length - 1) {
+        slideIndex = 0;
+    }
+    if (n < 0) {
+        slideIndex = worksImgList.length - 1;
+    }
+    slide.setAttribute('src', worksImgList[slideIndex].getAttribute('src'));
+}
+
+function nextSlide() {
+    showSlides(slideIndex += 1);
+}
+
+function previousSlide() {
+    showSlides(slideIndex -= 1);
+}
+
+function currentSlide(n) {
+    console.log("current slide n = ", n);
+    showSlides(slideIndex = n);
+}
